f5c8981f80c488670128d17973785f149c0531a4
elfinafrancis <69503303+elfinafrancis@users.noreply.github.com>
2020-12-18 15:52:40 +0530

    Update main.php

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 <?php
-include "conn.php"
+include "connection.php"
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Students</title>
+  <title>Student Details</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -14,37 +14,37 @@ include "conn.php"
 <body>
 <div class="container">
   <div class="col-lg-4">
-  <h2>Student Information</h2>
+  <h2>Student Details</h2>
   <form action="" name="form1" method="post">
     <div class="form-group">
       
     <div class="form-group">
-       <label for="pwd">Student ID:</label>
+       <label for="pwd">Student ID </label>
       <input type="text" class="form-control" id="sid" placeholder="Student ID" name="sid">
     </div>
     <div class="form-group">
-      <label for="pwd">Name:</label>
+      <label for="pwd">Name </label>
       <input type="text" class="form-control" id="name" placeholder="Name" name="name">
     </div>
     <div class="form-group">
-      <label for="pwd">Age:</label>
-      <input type="text" class="form-control" id="age" placeholder="Age" name="age">
+      <label for="pwd">Age </label>
+      <input type="tel" class="form-control" id="age" placeholder="Age" name="age">
     </div>
      <div class="form-group">
-      <label for="pwd">Gender:</label>
+      <label for="pwd">Gender </label>
       <input type="text" class="form-control" id="gender" placeholder="Gender" name="gender">
     </div>
     <div class="form-group">
-      <label for="pwd">Course:</label>
-      <input type="text" class="form-control" id="cname" placeholder="Enter Course Name" name="cname">
+      <label for="pwd">Course </label>
+      <input type="text" class="form-control" id="course" placeholder="Course" name="course">
     </div>
     <div class="form-group">
       <label for="pwd">Address:</label>
       <input type="text" class="form-control" id="add" placeholder="Address" name="address">
     </div>
-     <button type="submit" name="insert" class="btn btn-default">Insert</button>
-    <button type="submit" name="update" class="btn btn-default">Update</button>
-    <button type="submit" name="delete" class="btn btn-default">Delete</button>
+     <button type="submit" name="insert" class="btn btn-default">INSERT</button>
+    <button type="submit" name="update" class="btn btn-default">UPDATE</button>
+    <button type="submit" name="delete" class="btn btn-default">DELETE</button>
   </form>
   </div>
 </div>
@@ -53,14 +53,12 @@ include "conn.php"
 
   <thead>
       <tr>
-        
         <th>Student ID</th>
         <th>Name</th>
         <th>Age</th>
         <th>Gender</th>
         <th>Course</th>
         <th>Address</th>
-
       </tr>
     </thead>
     <tbody>
@@ -74,7 +72,7 @@ include "conn.php"
         echo "<td>"; echo $row["name"];echo "</td>";
         echo "<td>"; echo $row["age"];echo "</td>";
         echo "<td>"; echo $row["gender"];echo "</td>";
-        echo "<td>"; echo $row["cname"];echo "</td>";
+        echo "<td>"; echo $row["course"];echo "</td>";
         echo "<td>"; echo $row["address"];echo "</td>";
         echo "</tr>";
       }
@@ -89,7 +87,7 @@ include "conn.php"
 <?php
 if(isset($_POST["insert"]))
 {
-mysqli_query($link,"insert into stuinfo values('$_POST[sid]','$_POST[name]','$_POST[age]','$_POST[gender]','$_POST[cname]','$_POST[address]')");
+mysqli_query($link,"insert into stuinfo values('$_POST[sid]','$_POST[name]','$_POST[age]','$_POST[gender]','$_POST[course]','$_POST[address]')");
 ?>
 <script type="text/javascript">
   window.location.href= window.location.href;
